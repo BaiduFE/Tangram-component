b6e39b1849b336cc93c162bdc51f09c98956f32a
liujinlong <liujinlong@baidu.com>
2011-01-04 15:23:14 +0800

    修改用例将类型改成小写

--- test/baidu/ui/input/js/create.js
+++ test/baidu/ui/input/js/create.js
@@ -10,7 +10,7 @@ test("create input in container", function() {
 	var input = baidu.ui.input.create(te.dom[0].id);
 	var inputElement = input.getMain();
 	ok(inputElement, "dom created");
-	equals(input.uiType, "INPUT", "check type");
+	equals(input.uiType, "input", "check type");
 //	equals(input.getMain().id, div_test.id, "check input is inserted into target or not");
 	equals(input.getMain().id, te.dom[0].id, "check input is inserted into target or not");
 	te.dom.push(inputElement);
