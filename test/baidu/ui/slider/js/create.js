9dc983f8e651ba1b8e50a947ccccda0375fbee60
liujinlong <liujinlong@baidu.com>
2011-01-25 20:36:07 +0800

    添加异步代码，使得ie下第一次运行成功(防止未加载完)

--- test/baidu/ui/slider/js/create.js
+++ test/baidu/ui/slider/js/create.js
@@ -1,6 +1,14 @@
 module('baidu.ui.slider.create');
 
 test('init',function(){
-	var s  = baidu.ui.slider.create(te.dom[0]);
-	ok(s.getBody(),'check body');
-})
+	var div = document.body.appendChild(document.createElement("div"));
+	stop();
+	setTimeout(function(){
+		var s  = baidu.ui.slider.create(div);
+		var dd = s.getBody()!='object'&&s.getBody()!='undefined'
+		equal(s.getBody().className,"tangram-slider","check body class");
+		ok(dd,'check body');
+		start();
+	},30)
+
+});
