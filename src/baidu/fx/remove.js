e4e36cf3302250f1112a34f726b0a32fa7260b05
lixiaopeng <lixiaopeng@baidu.com>
2011-02-28 17:54:40 +0800

    修改baidu.fx.remove中onafterfinish被屏蔽的问题

--- src/baidu/fx/remove.js
+++ src/baidu/fx/remove.js
@@ -29,7 +29,12 @@
  */
 
 baidu.fx.remove = function(element, options) {
+    var onafterfinish = options[onafterfinish] ? options[onafterfinish] : new Function();
+    
     return baidu.fx.fadeOut(element, baidu.object.extend(options||{}, {
-        onafterfinish: function(){baidu.dom.remove(this.element);}
+        onafterfinish: function(){
+            baidu.dom.remove(this.element);
+            onafterfinish.call(this);
+        }
     }));
 };
