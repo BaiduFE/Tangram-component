f5d49ccbc3c92b31161e4d0b4bea97efd0ed2895
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-01 16:49:13 +0800

    update

---
+++ test/tools/br/geneHistory.php
@@ -0,0 +1,14 @@
+<?php
+
+function geneHistory($html){
+	$path = '/home/work/repos/report/ui';
+	if (!file_exists($path))
+	mkdir($path,0777,true);
+	date_default_timezone_set('PRC');
+	$time = date('Y-m-d-H-i-s');
+	$file_name = $path.'/'.$time.'.html';
+	$file_pointer = fopen($file_name, "w");
+	fwrite($file_pointer, $html);
+	fclose($file_pointer);
+}
+?>
