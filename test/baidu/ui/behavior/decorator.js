e9e8e088217c418d1384e3794780c83947d07d77
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-08 16:52:57 +0800

    添加装饰器行为用例

--- test/baidu/ui/behavior/decorator.js
+++ test/baidu/ui/behavior/decorator.js
@@ -1,5 +1,28 @@
 module('baidu.ui.behavior.decorator');
 
-test('base', function(){
-	ok(false, '用例需要补充');
+(function() {
+	te.getUI = function(options) {
+//		var div = document.body.appendChild(document.createElement('div'));
+//		div.id = 'id_decorator';
+//		te.dom.push(div);
+		
+		var ui = baidu.ui.createUI(function(options) {
+		}).extend({
+			uiType : 'testType',
+			decorator : true,
+			render : function(target) {
+				this.renderMain();
+				this.body = target;
+				this.getMain().appendChild(this.body);
+				this.dispatchEvent('onload');
+			}
+		});
+		return new ui();
+	};
+})();
+
+test('base', function() {
+	var ui = te.getUI();
+	stop();
+	ui.render(te.dom[0]);
 });
