b0e5c94cf73093b1fd6a22701258395218a65afd
lixiaopeng <lixiaopeng@baidu.com>
2011-03-14 16:38:10 +0800

    修改引用

--- src/baidu/ui/ColorPicker/ColorPicker$more.js
+++ src/baidu/ui/ColorPicker/ColorPicker$more.js
@@ -5,9 +5,10 @@
 
 ///import baidu.ui.ColorPicker;
 ///import baidu.ui.ColorPalette;
+
 ///import baidu.ui.Button;
 ///import baidu.ui.Dialog;
-///import baidu.ui.Dialog$button;
+///import baidu.ui.Dialog.Dialog$button;
 ///import baidu.ui.create;
 
 ///import baidu.object.extend;
