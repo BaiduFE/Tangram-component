cf0033a49a06a61dc74333dfddb45b046f8d8f2c
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-18 02:55:04 +0800

    data case

--- test/baidu/data/dataSource/get.php
+++ test/baidu/data/dataSource/get.php
@@ -1,5 +1,9 @@
 <?php
-if(array_key_exists("key", $_GET))
+if(array_key_exists('err', $_REQUEST))
+header('HTTP/1.1 500 error');
+if(array_key_exists("m", $_REQUEST))
+echo $_SERVER['REQUEST_METHOD'];
+else if(array_key_exists("key", $_GET))
 echo mktime();//返回一个当前时间，用于校验cache的有消息
 else
 echo "ajax!";
