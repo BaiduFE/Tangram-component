8081028e8d53ada09195ea7f3f1f73c7996f77ff
bellcliff <cliff_bell.cn@hotmail.com>
2011-03-20 17:59:03 +0800

    更新colorPicker用例，待确认后依然需要补充更多关于操作相关的用例

--- test/baidu/ui/ColorPicker.js
+++ test/baidu/ui/ColorPicker.js
@@ -1,14 +1,11 @@
 module('baidu.ui.ColorPicker');
-ua.importsrc("baidu.object.extend");
-
-
 (function() {
 	function mySetup() {
 		var div = document.createElement('div');
 		div.id = 'div_test';
 		document.body.appendChild(div);
 		var b1 = document.createElement('input');
-        b1.id = 'b1';
+		b1.id = 'b1';
 		b1.type = "button";
 		b1.value = "test";
 		div.appendChild(b1);
@@ -18,43 +15,46 @@ ua.importsrc("baidu.object.extend");
 
 	var s = QUnit.testStart;
 	QUnit.testStart = function() {
-		s.apply(this, arguments);;
+		s.apply(this, arguments);
 		mySetup();
-	}
+	};
 })();
 
 function createColorPicker(options) {
-    var colorPicker = baidu.ui.create(baidu.ui.ColorPicker,
-    baidu.object.extend({
-        autoRender: true,
-        element: testingElement.dom[0]
-    }, options || {}));
-    testingElement.obj.push(colorPicker);
-    return colorPicker;
+	options = options || {};
+	options.autoRender = true;
+	options.element = te.dom[0];
+	var colorPicker = baidu.ui.create(baidu.ui.ColorPicker, options);
+	te.obj.push(colorPicker);
+	return colorPicker;
 }
 
 test("create colorPicker and open", function() {
-    var colorPicker = createColorPicker();
-    colorPicker.open();
-    ok(isShown(colorPicker.getBody()), 'colorPicker is shown');
+	stop();
+	ua.loadcss(upath + 'ColorPicker/css/colorpicker.css', function() {
+		var colorPicker = createColorPicker();
+		equal(colorPicker.uiType, 'colorpicker', 'check uitype');
+		colorPicker.open();
+		ok(isShown(colorPicker.getBody()), 'colorPicker is shown');
+		start();
+	}, 'tangram-dialog-cancel', 'height', '20px');
 });
 
 test("onchosen", function() {
-    var colorPicker = createColorPicker({
-        onchosen: function(colorObj) {
-            ok(true, 'color chosen');
-            equal('#B22222', colorObj.color);
-        }
-    });
-    colorPicker.open();
-    $('#' + colorPicker.getId('B22222')).click();
+	var colorPicker = createColorPicker({
+		onchosen : function(colorObj) {
+			ok(!isShown(colorPicker.getBody()), 'colorPicker closed');
+			equal('#B22222', colorObj.color);
+		}
+	});
+	colorPicker.open();
+	$('#' + colorPicker.getId('B22222')).click();
 });
 
 test("dispose", function() {
-  var colorPicker = createColorPicker(),
-      colorPickerId = colorPicker.getId();
-  colorPicker.open();
-  colorPicker.dispose();
-  equals(baidu.g(colorPickerId), null,
-          "Check colorPicker element exists or not");
-})
+	var colorPicker = createColorPicker(), colorPickerId = colorPicker.getId();
+	colorPicker.open();
+	colorPicker.dispose();
+	equals(baidu.g(colorPickerId), null,
+			"Check colorPicker element exists or not");
+});
