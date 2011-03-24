a0e98cce0907206fa21e26e091302bf99b97a020
liujinlong <liujinlong@baidu.com>
2011-03-24 14:14:41 +0800

    增加用例，修改用例实现，增加用例描述

--- test/baidu/ui/ScrollBar.js
+++ test/baidu/ui/ScrollBar.js
@@ -9,6 +9,9 @@ module('baidu.ui.ScrollBar');
  * dispose
 */
 
+/**
+ * render 提供渲染功能，创建3个对象，prev（button） slider next（button） ,这三个组成滚动条
+ */
 test('init render',function(){
 	stop();
 	ua.loadcss(upath+'ScrollBar/style.css',function(){
@@ -17,7 +20,7 @@ test('init render',function(){
 			.css('color', 'black');
 		div.id = "div_test";
 		var options = {
-			skin: 'scrollbar',
+			skin: 'scrollbar-a',
 			onload : function(){
 				ok(true,'load is success');
 			}
@@ -28,61 +31,73 @@ test('init render',function(){
 		equal(scrollbar._prev.uiType,'button',"check prev type");
 		equal(scrollbar._next.uiType,'button','check next type');
 		equal(scrollbar._slider.uiType,'slider','check _slider type');
-		equal(scrollbar.getSize().height,scrollbar._next.getBody().offsetHeight);
-		equal(scrollbar._slider.getThumb().innerHTML.match(scrollbar.getThumbString()),scrollbar.getThumbString(),"check thumb inner");
-		equal(scrollbar._slider.getMain().style['height'],(200-2*scrollbar._arrowSize['height'])+'px','check slider height');
+		equal(scrollbar.getSize().height,200,'check scrollbar height');
+		var nextwidth  = baidu.dom.getStyle(scrollbar._next.getBody(),'width'),
+		    nextheight = baidu.dom.getStyle(scrollbar._next.getBody(),'height'),
+		    prevheight = baidu.dom.getStyle(scrollbar._prev.getBody(),'height');
+		equal(scrollbar.getSize().width+'px',nextwidth,'check scrollbar width');
+		equal(scrollbar._slider.getMain().style['height'],(200-parseInt(nextheight)-parseInt(prevheight))+'px','check slider height');
 		equal(baidu.dom.getStyle(scrollbar._slider.getThumb(),'height'),scrollbar.dimension+'%','check thumb height');
-		
-//		alert(baidu.dom.getStyle(scrollbar._prev.getBody(),'height')); prev 的高度怎么变成0了
-		start();
 		te.dom.push(div);
+		start();
 	});
 
 });
 
+/**
+ * 验证分别鼠标点击 前后按钮时，滑块移动到像素值
+ */
 test('mousedown prev next',function(){
-    var div = document.body.appendChild(document.createElement("div"));
-	$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
-			.css('color', 'black');
-	div.id = "div_test";
-	var options = {
-		skin: 'scrollbar',
-		onmousedown : function(){
-			ok(true,'onmousedown event');
-		}
-	};
-	var scrollbar = new baidu.ui.ScrollBar(options);
-	scrollbar.render(div);
-	var thumb = scrollbar._slider.getThumb();
-	var thumbtop = parseInt(baidu.dom.getStyle(thumb,'top'));
-	var sliderheight = parseInt(baidu.dom.getStyle(scrollbar._slider.getMain(),'height'));
-	var thumbheight = sliderheight*parseFloat(baidu.dom.getStyle(thumb,'height'))*0.01;
-	ua.mousedown(scrollbar._next.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'top'),(thumbtop+sliderheight-thumbheight)*(scrollbar.step)*0.01+'px','click next step 1');
-	
-	ua.mousedown(scrollbar._next.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	ua.mousedown(scrollbar._next.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'top'),(thumbtop+sliderheight-thumbheight)*(scrollbar.step*3)*0.01+'px','click next step 3');
-	
-	ua.mousedown(scrollbar._prev.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'top'),(thumbtop+sliderheight-thumbheight)*(scrollbar.step*2)*0.01+'px','click prev step 1');
-	
-	ua.mousedown(scrollbar._slider.getThumb());
-	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'top'),(thumbtop+sliderheight-thumbheight)*(scrollbar.step*2)*0.01+'px','click thumb ');
-	
-	ua.mousedown(scrollbar._prev.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	ua.mousedown(scrollbar._prev.getBody());
-	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'top'),thumbtop+'px','click prev step 3');
-    te.dom.push(div);
+	stop();
+	ua.loadcss(upath+'ScrollBar/style.css',function(){
+	    var div = document.body.appendChild(document.createElement("div"));
+		$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
+				.css('color', 'black');
+		div.id = "div_test";
+		var options = {
+			skin: 'scrollbar-a',
+			onmousedown : function(){
+				ok(true,'onmousedown event');
+			}
+		};
+		var scrollbar = new baidu.ui.ScrollBar(options);
+		scrollbar.render(div);
+		var thumb = scrollbar._slider.getThumb();
+		var thumbtop = parseInt(baidu.dom.getStyle(thumb,'top'));
+		var sliderheight = parseInt(baidu.dom.getStyle(scrollbar._slider.getMain(),'height'));
+		var thumbheight = sliderheight*parseFloat(baidu.dom.getStyle(thumb,'height'))*0.01;
+		
+		ua.mousedown(scrollbar._next.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		equal(baidu.dom.getStyle(thumb,'top'),Math.round((thumbtop+sliderheight-thumbheight)*(scrollbar.step)*0.01)+'px','click next step 1');
+		
+		ua.mousedown(scrollbar._next.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		ua.mousedown(scrollbar._next.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		equal(baidu.dom.getStyle(thumb,'top'),Math.round((thumbtop+sliderheight-thumbheight)*(scrollbar.step*3)*0.01)+'px','click next step 3');
+		
+		ua.mousedown(scrollbar._prev.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		equal(baidu.dom.getStyle(thumb,'top'),Math.round((thumbtop+sliderheight-thumbheight)*(scrollbar.step*2)*0.01)+'px','click prev step 1');
+		
+		ua.mousedown(scrollbar._slider.getThumb());
+		ua.mouseup(scrollbar._next.getBody());
+		equal(baidu.dom.getStyle(thumb,'top'),Math.round((thumbtop+sliderheight-thumbheight)*(scrollbar.step*2)*0.01)+'px','click thumb ');
+		
+		ua.mousedown(scrollbar._prev.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		ua.mousedown(scrollbar._prev.getBody());
+		ua.mouseup(scrollbar._next.getBody());
+		equal(baidu.dom.getStyle(thumb,'top'),Math.round(thumbtop)+'px','click prev step 3');
+	    te.dom.push(div);
+	    start();
+   });
 });
 
+/**
+ * 验证横向时 点击前后按钮时的情况
+ */
 test('horizontal mousedown prev next',function(){
     var div = document.body.appendChild(document.createElement("div"));
 	$(div).css('width', '200px').css('height', '100px').css('border', 'solid')
@@ -103,30 +118,33 @@ test('horizontal mousedown prev next',function(){
 	var thumbwidth = sliderwidth*parseFloat(baidu.dom.getStyle(thumb,'width'))*0.01;
 	ua.mousedown(scrollbar._next.getBody());
 	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'left'),(thumbleft+sliderwidth-thumbwidth)*(scrollbar.step)*0.01+'px','click next step 1');
+	equal(baidu.dom.getStyle(thumb,'left'),Math.round((thumbleft+sliderwidth-thumbwidth)*(scrollbar.step)*0.01)+'px','click next step 1');
 	
 	ua.mousedown(scrollbar._next.getBody());
 	ua.mouseup(scrollbar._next.getBody());
 	ua.mousedown(scrollbar._next.getBody());
 	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'left'),(thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*3)*0.01+'px','click next step 3');
+	equal(baidu.dom.getStyle(thumb,'left'),Math.round((thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*3)*0.01)+'px','click next step 3');
 	
 	ua.mousedown(scrollbar._prev.getBody());
 	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'left'),(thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*2)*0.01+'px','click prev step 1');
+	equal(baidu.dom.getStyle(thumb,'left'),Math.round((thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*2)*0.01)+'px','click prev step 1');
 	
 	ua.mousedown(scrollbar._slider.getThumb());
 	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'left'),(thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*2)*0.01+'px','click thumb ');
+	equal(baidu.dom.getStyle(thumb,'left'),Math.round((thumbleft+sliderwidth-thumbwidth)*(scrollbar.step*2)*0.01)+'px','click thumb ');
 	
 	ua.mousedown(scrollbar._prev.getBody());
 	ua.mouseup(scrollbar._next.getBody());
 	ua.mousedown(scrollbar._prev.getBody());
 	ua.mouseup(scrollbar._next.getBody());
-	equal(baidu.dom.getStyle(thumb,'left'),thumbleft+'px','click prev step 3');
+	equal(baidu.dom.getStyle(thumb,'left'),Math.round(thumbleft)+'px','click prev step 3');
     te.dom.push(div);
 });
 
+/**
+ * 验证拖动滑块时，滑块像素值与鼠标拖动的距离之间的对应值
+ */
 test('mousemove thumb',function(){
     var div = document.body.appendChild(document.createElement("div"));
 	$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
@@ -139,7 +157,7 @@ test('mousemove thumb',function(){
 		    equal(baidu.dom.getStyle(scrollbar._slider.getThumb(),'top'),(thumbtop+50)+'px','drag thumb 50');
             var value = 50/slidelength * 100;
             equal(scrollbar._slider.value,value,'check slider value');
-            te.dom.push(div);
+            te.obj.push(scrollbar);
 		    start();
 		}
 	};
@@ -174,6 +192,9 @@ test('mousemove thumb',function(){
 	}, 60);
 });
 
+/**
+ * 鼠标点在滑动距离内时，滑块跟着移动的最终坐标值
+ */
 test('mousedown slider',function(){
     var div = document.body.appendChild(document.createElement("div"));
 	$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
@@ -211,6 +232,9 @@ test('mousedown slider',function(){
 	},100)
 });
 
+/**
+ * flushUI的作用：设定_scrollBarSize值，设置滑动距离（总长度减去两个按钮的长度），跟进dimension和滑动距离设置滑块长度
+ */
 test('flushUI',function(){
     var div = document.body.appendChild(document.createElement("div"));
 	$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
@@ -249,6 +273,11 @@ test('flushUI',function(){
 //
 //});
 
+/**
+ * dispose作用：删除生成的html元素，删除生成事件，这里通过执行两个button ，一个 slider  dispose()方法来完成dispose操作，实现思路没问题
+ * 但是经验证，事件并没有完全删除，因此提了bug：PUBLICGE-343。分析是由于Button，Slider的dispose（）没有完全删除事件
+ * 
+ */
 test('dispose',function(){
     var div = document.body.appendChild(document.createElement("div"));
 	$(div).css('width', '100px').css('height', '200px').css('border', 'solid')
@@ -259,6 +288,7 @@ test('dispose',function(){
 			ok(true,"it's dispose");
 		}
 	};
+	var l1 = baidu.event._listeners.length;
 	var scrollbar = new baidu.ui.ScrollBar(options);
 	scrollbar.render(div);
 	scrollbar.dispose();
@@ -266,6 +296,6 @@ test('dispose',function(){
     ok(!scrollbar._slider,'check _slider not exist');
     ok(!scrollbar._next,'check next not exist');
 	ok(!scrollbar._prev,'check prev not exist');
-	
+	equals(baidu.event._listeners.length, l1, 'event removed all');
 });
 
