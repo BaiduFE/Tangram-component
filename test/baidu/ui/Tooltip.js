de4acf0b3809f53793fe3fa64dc173202ebca5b0
liujinlong <liujinlong@baidu.com>
2011-03-26 20:54:05 +0800

    用例 Slider 修改警告； Toolbar 修改用例  enable； Tooltip 增加用例 target title 和 dispose

--- test/baidu/ui/Tooltip.js
+++ test/baidu/ui/Tooltip.js
@@ -5,7 +5,6 @@ module('baidu.ui.Tooltip');
 		for ( var i = 0; i < 3; i++) {
 			var div = document.createElement('div');
 			div.id = "div_test" + i;
-			div.innerHTML = "div_test" + i;
 			document.body.appendChild(div);
 			te.dom.push(div);
 		}
@@ -17,29 +16,37 @@ module('baidu.ui.Tooltip');
 	}
 })();
 
+/**
+ * target ?
+ */
+
 test('target and type', function() {
 	var div = testingElement.dom[0];
-	var t = new baidu.ui.Tooltip();
+	var t = new baidu.ui.Tooltip({});
 	t.render(div);
 	equals(t.uiType, 'tooltip', 'check type');
-	equals(t.targetId, div.id, 'check id');
+	equals(t.getMain().id, div.id, 'check id');
 });
 
-test(
-		'option attribute',
-		function() {
+test('option attribute',function() {
 			var step = 0, tp, t, div = testingElement.dom[0];
 			div.style.position = 'absolute';
 			div.style.left = '100px';
 			div.style.height = '100px';
 			div.style.top = '100px';
+			var target = document.body.appendChild(document.createElement('div'));
+			target.id = 'targetid';
+			target.title = 'test target';
+			var content = document.createElement('span');
+			    content.innerHTML = '提示';
 			var op = {
 				width : 100,
 				height : 100,
-				content : 'test',
 				single : true,
 				zIndex : 1001,
 				positionBy : 'element',
+				target : target,
+				content : content,
 				onopen : function() {
 					step++;
 					tp = t.getMain();
@@ -55,7 +62,6 @@ test(
 						equals(tp.style.zIndex, op.zIndex, 'check dom z index');
 						equals(t.positionBy, op.positionBy, 'check position by');
 						t.close();
-
 						t.update( {
 							positionBy : 'mouse'
 						});
@@ -84,10 +90,14 @@ test('on', function() {
 			equals(_step++, step, 'check step');
 		};
 	};
+	var target = document.body.appendChild(document.createElement('div'));
+		target.id = 'targetid',
+		target.title = 'test target';
 	var tp = new baidu.ui.Tooltip( {
 		// oninit : handle('oninit', 0),
+		target : target,
 		onbeforeopen : handle('onbeforeopen', 1 - 1),
-		onopen : handle('onopen', 2 - 1),
+		onopen : handle('open', 2 - 1),
 		onbeforeclose : handle('onbeforeclose', 3 - 1),
 		onclose : handle('onclose', 4 - 1)
 	});
@@ -99,35 +109,173 @@ test('on', function() {
 test("Set or don't set the offset of the tooltip", function(){
 	var div = testingElement.dom[0];
 	div.style.position = 'absolute';
-	div.style.left = '100px';
 	div.style.height = '100px';
-	div.style.top = '100px';
+	div.style.width = '100px';
+	var target = document.body.appendChild(document.createElement('div'));
+		target.id = 'targetid';
+		$(target).css('height','50px').css('width','50px').css('left','0px').css('top','0px').css('color','red');
+		var targetPos = baidu.dom.getPosition(target);
 	var tp = new baidu.ui.Tooltip({
-		content : 'tooltip',
-		offset : [100,100]
+		target : target,
+		offset : [100,100],
+		contentText : 'content'
 	});
 	tp.render(div);
 	tp.open();
-	equals(tp.getMain().style.top, '300px', 
+	equals(tp.getMain().style.top, targetPos.top+50+100+'px', 
 			"If set the offset, the top position of the "
-			+"tooltip = the top position of the div(100px) + the "
-			+"height of the div(100px) + the offset of the tooltip(100px) ");
-	equals(tp.getMain().style.left, '200px', 
+			+"tooltip = the top position of the div + the "
+			+"height of the div + the offset of the tooltip ");
+	equals(tp.getMain().style.left, targetPos.left+50+100+'px', 
 			"If set the offset, the left position of the "
-			+"tooltip = the left position of the div(100px) + the "
-			+"offset of the tooltip(100px) ");
+			+"tooltip = the left position of the div + the "
+			+"offset of the tooltip ");
 	tp.close();
+	var content = document.createElement('span');
+    content.innerHTML = '提示';
 	var tp1 = new baidu.ui.Tooltip({
-		content : 'tooltip_1'
+		target : target,
+		content : content
 	});
 	tp1.render(div);
 	tp1.open();
-	equals(tp1.getMain().style.top, '200px', 
+	equals(tp1.getMain().style.top, targetPos.top+50+'px', 
 			"If don't set the offset, the top position of the "
-			+"tooltip = the top position of the div(100px) + the "
-			+"height of the div(100px) ");
-	equals(tp1.getMain().style.left, '100px', 
+			+"tooltip = the top position of the div + the "
+			+"height of the div ");
+	equals(tp1.getMain().style.left, targetPos.left+50+'px', 
 			"If don't set the offset, the left position of the "
-			+"tooltip = the left position of the div(100px) ");
+			+"tooltip = the left position of the div ");
 	tp1.close();
+});
+
+/*
+ * 设置多例的情况,默认情况
+ */
+test('isSingleton false',function(){
+	var div = testingElement.dom[0];
+	var target1 = document.body.appendChild(document.createElement('div')),
+	    target2 = document.body.appendChild(document.createElement('div'));
+	    
+	var tp = new baidu.ui.Tooltip({
+		target : [target1,target2]
+	});
+	tp.render(div);
+	
+	//同一组内显示
+	tp.open(target1);
+	equal(tp.currentTarget,target1,'check currentTarget target1');//验证currentTarget，因tootip位置根据currentTarget定位
+	ok(parseInt(tp.getMain().style.left)>0,'tooltip show');
+	tp.open(target2);
+	equal(tp.currentTarget,target2,'check currentTarget target2');
+	ok(parseInt(tp.getMain().style.left)>0,'tooltip show');
+	tp.close();
+	ok(!(parseInt(tp.getMain().style.left)>0),'tooltip close');
+	equal(tp.currentTarget,null,'check currentTarget null');
+	
+	//不同组显示
+	var div1 = document.body.appendChild(document.createElement('div'));
+	var target3 = document.body.appendChild(document.createElement('div')); 
+	var tp1 = new baidu.ui.Tooltip({
+		target : target3
+	});
+	tp1.render(div1);
+	tp1.open(target3);
+	tp.open(target2);
+	ok(parseInt(tp.getMain().style.left)>0,'tooltip show');
+	ok(parseInt(tp1.getMain().style.left)>0,'tooltip1 show');
+	equal(tp.currentTarget,target2,'check tp currentTarget target2');
+	equal(tp1.currentTarget,target3,'check tp1 currentTarget target3');
+
+});
+
+/*
+ * 单例情况，不同组只能有一个tooltip显示
+ */
+test('isSingleton true',function(){
+	var div = testingElement.dom[0];
+	var target1 = document.body.appendChild(document.createElement('div')),
+	    target2 = document.body.appendChild(document.createElement('div'));
+	target1.id = 'test1';
+	target2.id = 'test2';    
+	var tp = new baidu.ui.Tooltip({
+		target : [target1,target2]
+	});
+	baidu.ui.Tooltip.isSingleton = true;
+	tp.render(div);
+	
+	//不同组显示
+	var div1 = document.body.appendChild(document.createElement('div'));
+	var target3 = document.body.appendChild(document.createElement('div')); 
+	target3.id = 'test3';
+	var tp1 = new baidu.ui.Tooltip({
+		target : [target3]
+	});
+	tp1.render(div1);
+	
+	tp.open(target2);
+	tp1.open();
+	ok(!(parseInt(tp.getMain().style.left)>0),'tooltip close')
+	ok(parseInt(tp1.getMain().style.left)>0,'tooltip1 show');
+	equal(tp.currentTarget,null,'check tp currentTarget target2');
+	equal(tp1.currentTarget,target3,'check tp1 currentTarget target3');
+
+});
+
+/**
+ * target设置title时，优先级：contentElement>content>title
+ * 不提供contentElement更新功能？所有target使用同一个提示信息？
+ */
+test('target title',function(){
+	var div = testingElement.dom[0],
+	    target1 = document.body.appendChild(document.createElement('div')),
+	    contentElement = document.createElement('span');
+	target1.id = 'target1';
+	target1.title = 'this is a title';  
+	contentElement.innerHTML = '提示';
+	var options = {
+		target : [target1]
+	}; 
+	var tp = new baidu.ui.Tooltip(options);
+	tp.render(div);
+	equal(baidu.getAttr(tp.target[0], 'tangram-tooltip-title'),'this is a title');
+	ok(!tp.getTarget().title,'cancel title');
+	tp.open();
+	equal(tp.getBody().innerHTML,'this is a title');
+	options = {//设置contentElement ，应该显示contentElement
+		contentElement : contentElement,
+		content : 'content'
+	}
+	tp.update(options);
+	equal(tp.getBody().firstChild,contentElement);
+	options = {//设置contentElement=null，应该显示content
+		contentElement : null,
+		content : 'content'
+	}
+	tp.update(options);
+	equal(tp.getBody().innerHTML,'content');
+	options = {//重新设置回contentElement，则应显示contentElement
+		contentElement : contentElement,
+		content : 'content'
+	}
+	tp.update(options);
+	equal(tp.getBody().firstChild,contentElement);
+
+});
+
+test('dispose',function(){
+	var l1 = baidu.event._listeners.length;
+	var div = testingElement.dom[0];
+	var target = document.body.appendChild(document.createElement('div'));
+	var options = {
+		target:target,
+		ondispose : function(){
+			ok(true,'dispose is dispatched');
+		}
+	};
+	var t = new baidu.ui.Tooltip(options);
+	t.render(div);
+	t.dispose();
+	equal(baidu.dom.g(t.getId()),null,'disposed');
+	equals(baidu.event._listeners.length, l1, 'event removed all');
 });
