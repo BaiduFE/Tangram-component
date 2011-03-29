27d9fa0f2ee8bfc5128e4ff43a9ce55df7401021
berg <cnberg@gmail.com>
2011-03-29 16:12:45 +0800

    修改createUI的注释

--- src/baidu/ui/createUI.js
+++ src/baidu/ui/createUI.js
@@ -1,11 +1,6 @@
 /*
  * Tangram UI
  * Copyright 2009 Baidu Inc. All rights reserved.
- *
- * path: baidu/ui/createUI.js
- * author: berg
- * version: 1.1.0
- * date: 2010/12/02
  */
 
 ///import baidu.ui;
