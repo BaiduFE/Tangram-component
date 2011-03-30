7e8620e59c7f3da679df666bd7654b74b7a73c46
tianlili <tianlili3283@gmail.com>
2011-03-30 14:11:12 +0800

    补充用例

--- test/baidu/ui/Carousel/Carousel$autoScroll.js
+++ test/baidu/ui/Carousel/Carousel$autoScroll.js
@@ -52,126 +52,126 @@ test("autoScrollTo:H, index=1, time=4, type=item", function() {
 	},450);
 });
 
-//test("autoScrollTo:H, index=1, time=8, type=item", function() {
-//    var cas, oitem, item, c;
-//    cas = createCarousel(te.dom[0], false);
-//	cas.render();
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollLeft, item.offsetWidth*6+2,"验证滚动距离是否为六个item的width+2（到最后一个item就停止）");
-//		equals(cas.scrollIndex, 8,"验证滚动到item8（不循环滚动）");
-//		cas.dispose();
-//		start();
-//	},890);
-//});
-//
-//test("autoScrollTo:H, index=1, time=8, type=item, isCycle:true", function() {
-//	var cas, oitem, item, c;
-//	cas = createCarousel(te.dom[0], false);
-//	cas.isCycle = true;
-//	cas.render();
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollLeft, 0,"验证滚动距离是否为0（循环滚动）");
-//		equals(cas.scrollIndex, 0,"验证滚动回item0（循环滚动）");
-//		cas.dispose();
-//		start();
-//	},890);
-//});
-//
-//test("autoScrollTo:V, index=2, time=2, type=item", function() {
-//	var cas, oitem, item, c;
-//	cas = createCarousel(te.dom[0], true);
-//	cas.render();
-//  item = document.getElementById(cas.getId("item" + 2));
-//  c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(2);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollTop, item.offsetHeight*4, "验证滚动距离是否为四个item的height");
-//		cas.dispose();
-//		start();
-//	},250);
-//});
-//
-//test("autoScrollTo:H, index=1, time=2, type=page", function() {
-//    var cas, oitem, item, c;
-//    cas = createCarousel(te.dom[0], false);
-//	cas.render();
-//	cas.autoScroll.type = 'page';
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollLeft, item.offsetWidth*6,"验证滚动距离是否为两页item的width");
-//		equals(cas.scrollIndex, 6,"验证滚动到item6");
-//		cas.dispose();
-//		start();
-//	},250);
-//});
-//
-//test("autoScrollTo:H, index=3, time=2, type=page", function() {
-//    var cas, oitem, item, c;
-//    cas = createCarousel(te.dom[0], false);
-//	cas.render();
-//	cas.autoScroll.type = 'page';
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(3);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollLeft, item.offsetWidth*6,"验证滚动距离是否为两页item的width");
-//		equals(cas.scrollIndex, 6,"验证滚动到item6（不循环滚动）");
-//		cas.dispose();
-//		start();
-//	},250);
-//});
-//
-//test("autoScrollTo:H, index=3, time=2, type=page", function() {
-//    var cas, oitem, item, c;
-//    cas = createCarousel(te.dom[0], false);
-//    cas.isCycle = true;
-//	cas.render();
-//	cas.autoScroll.type = 'page';
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(3);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollLeft, 0,"验证滚动距离是否为0（循环滚动）");
-//		equals(cas.scrollIndex, 0,"验证滚动回item0（循环滚动）");
-//		cas.dispose();
-//		start();
-//	},250);
-//});
-//
-//test("autoScrollTo:V, index=1, time=2, type=page", function() {
-//    var cas, oitem, item, c;
-//    cas = createCarousel(te.dom[0], true);
-//	cas.render();
-//	cas.autoScroll.type = 'page';
-//	item = document.getElementById(cas.getId("item" + 2));
-//	c = document.getElementById(cas.getId());
-//	cas.focus(0);
-//	stop();
-//	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
-//	setTimeout(function(){
-//		equals(c.scrollTop, item.offsetHeight*6,"验证滚动距离是否为两页item的width");
-//		equals(cas.scrollIndex, 6,"验证滚动到item6");
-//		cas.dispose();
-//		start();
-//	},250);
-//});
+test("autoScrollTo:H, index=1, time=8, type=item", function() {
+    var cas, oitem, item, c;
+    cas = createCarousel(te.dom[0], false);
+	cas.render();
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollLeft, item.offsetWidth*6+2,"验证滚动距离是否为六个item的width+2（到最后一个item就停止）");
+		equals(cas.scrollIndex, 8,"验证滚动到item8（不循环滚动）");
+		cas.dispose();
+		start();
+	},890);
+});
+
+test("autoScrollTo:H, index=1, time=8, type=item, isCycle:true", function() {
+	var cas, oitem, item, c;
+	cas = createCarousel(te.dom[0], false);
+	cas.isCycle = true;
+	cas.render();
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollLeft, 0,"验证滚动距离是否为0（循环滚动）");
+		equals(cas.scrollIndex, 0,"验证滚动回item0（循环滚动）");
+		cas.dispose();
+		start();
+	},890);
+});
+
+test("autoScrollTo:V, index=2, time=2, type=item", function() {
+	var cas, oitem, item, c;
+	cas = createCarousel(te.dom[0], true);
+	cas.render();
+  item = document.getElementById(cas.getId("item" + 2));
+  c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(2);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollTop, item.offsetHeight*4, "验证滚动距离是否为四个item的height");
+		cas.dispose();
+		start();
+	},250);
+});
+
+test("autoScrollTo:H, index=1, time=2, type=page", function() {
+    var cas, oitem, item, c;
+    cas = createCarousel(te.dom[0], false);
+	cas.render();
+	cas.autoScroll.type = 'page';
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollLeft, item.offsetWidth*6,"验证滚动距离是否为两页item的width");
+		equals(cas.scrollIndex, 6,"验证滚动到item6");
+		cas.dispose();
+		start();
+	},250);
+});
+
+test("autoScrollTo:H, index=3, time=2, type=page", function() {
+    var cas, oitem, item, c;
+    cas = createCarousel(te.dom[0], false);
+	cas.render();
+	cas.autoScroll.type = 'page';
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(3);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollLeft, item.offsetWidth*6,"验证滚动距离是否为两页item的width");
+		equals(cas.scrollIndex, 6,"验证滚动到item6（不循环滚动）");
+		cas.dispose();
+		start();
+	},250);
+});
+
+test("autoScrollTo:H, index=3, time=2, type=page", function() {
+    var cas, oitem, item, c;
+    cas = createCarousel(te.dom[0], false);
+    cas.isCycle = true;
+	cas.render();
+	cas.autoScroll.type = 'page';
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(3);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollLeft, 0,"验证滚动距离是否为0（循环滚动）");
+		equals(cas.scrollIndex, 0,"验证滚动回item0（循环滚动）");
+		cas.dispose();
+		start();
+	},250);
+});
+
+test("autoScrollTo:V, index=1, time=2, type=page", function() {
+    var cas, oitem, item, c;
+    cas = createCarousel(te.dom[0], true);
+	cas.render();
+	cas.autoScroll.type = 'page';
+	item = document.getElementById(cas.getId("item" + 2));
+	c = document.getElementById(cas.getId());
+	cas.focus(0);
+	stop();
+	cas.scrollTo(1);//scrollLeft 表示滚动条向右拖动的距离 $(c).css('height')
+	setTimeout(function(){
+		equals(c.scrollTop, item.offsetHeight*6,"验证滚动距离是否为两页item的width");
+		equals(cas.scrollIndex, 6,"验证滚动到item6");
+		cas.dispose();
+		start();
+	},250);
+});
