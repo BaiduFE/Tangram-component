c47072589ff7b81ad407d614c58ca639e722a783
tianlili <tianlili3283@gmail.com>
2011-03-31 12:54:02 +0800

    修改加载css的方式，解决在IE和FF下用例不通过的情况

--- test/baidu/ui/Carousel/tools.js
+++ test/baidu/ui/Carousel/tools.js
@@ -11,25 +11,25 @@
 		m1.id = 'm1';
 		m1.innerHTML = 'm1';
 		m.appendChild(m1);
-
-		 var link = document.createElement("link");
-		 link.setAttribute("rel", "stylesheet");
-		 link.setAttribute("type", "text/css");
-		 link.setAttribute("href",
-		 '../../baidu/ui/Carousel/style.css');
-		 document.getElementsByTagName("head")[0].appendChild(link);
-
-//		var sheet = document.createElement("div");
-//		document.getElementsByTagName("head")[0].appendChild(sheet);
-//		var str = "<syle>"+
-//		    ".tangram-carousel {position: relative; overflow: hidden;	border: green solid 4px;}"
-//			+ ".tangram-carousel .tangram-carousel-scroll {position: absolute;border : blue solid 1px;}'"
-//			+ ".tangram-carousel .tangram-carousel-scroll .tangram-carousel-item {position: relatiev;float: left;border: pink solid 1px;width: 80px;}"
-//			+ ".tangram-carousel .tangram-carousel-scroll .tangram-carousel-item-focus {border: red solid 1px;background: #eee;}"
-//			+ ".tangram-carousel-btn-base {float: left;}"
-//			+ "td {border:solid 1px red;}"
-//		    + "</style>";
-//		sheet.innerHTML = str;
+//
+//		 var link = document.createElement("link");
+//		 link.setAttribute("rel", "stylesheet");
+//		 link.setAttribute("type", "text/css");
+//		 link.setAttribute("href",
+//		 '../../baidu/ui/Carousel/style.css');
+//		 document.getElementsByTagName("head")[0].appendChild(link);
+//
+////		var sheet = document.createElement("div");
+////		document.getElementsByTagName("head")[0].appendChild(sheet);
+////		var str = "<syle>"+
+////		    ".tangram-carousel {position: relative; overflow: hidden;	border: green solid 4px;}"
+////			+ ".tangram-carousel .tangram-carousel-scroll {position: absolute;border : blue solid 1px;}'"
+////			+ ".tangram-carousel .tangram-carousel-scroll .tangram-carousel-item {position: relatiev;float: left;border: pink solid 1px;width: 80px;}"
+////			+ ".tangram-carousel .tangram-carousel-scroll .tangram-carousel-item-focus {border: red solid 1px;background: #eee;}"
+////			+ ".tangram-carousel-btn-base {float: left;}"
+////			+ "td {border:solid 1px red;}"
+////		    + "</style>";
+////		sheet.innerHTML = str;
 		testingElement.dom.push(m1);
 		testingElement.dom.push(m);
 		testingElement.dom.push(div);
