9b7280bbfa71e99b40e5220e0ce584c3e8c2dd69
bellcliff <cliff_bell.cn@hotmail.com>
2011-04-21 01:35:23 +0800

    update case for widget

--- test/baidu/widget/dialog.js
+++ test/baidu/widget/dialog.js
@@ -4,12 +4,12 @@ baidu.widget.create("dialog", function(require, exports){
 	exports.create = function(){
 		return 'dialog_create';
 		//alert('creating dialog');
-	}
+	};
 },{depends:['uibase']});
 
 baidu.widget.create('dialogBase', function(require, exports){
 	exports.create = function(){
 		return 'dialogBase_create';
 		//alert('creating dialogBase');
-	}
+	};
 },{depends:['uibase']});
--- test/baidu/widget/uibase.js
+++ test/baidu/widget/uibase.js
@@ -1,5 +1,5 @@
 baidu.widget.create("uibase", function(require, exports){
 	exports.create = function(){
 		return 'uibase_create';
-	}
+	};
 });
