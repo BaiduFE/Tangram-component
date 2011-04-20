55892287e9a4ba552a5c04d12ac4b7974628079c
linlingyu <linlingyu@baidu.com>
2011-04-20 17:26:21 +0800

    增加边界识别功能

--- src/baidu/ui/behavior/posable/setPositionByElement.js
+++ src/baidu/ui/behavior/posable/setPositionByElement.js
@@ -5,6 +5,10 @@
 
 ///import baidu.ui.behavior.posable;
 ///import baidu.dom.getPosition;
+///import baidu.page.getScrollLeft;
+///import baidu.page.getScrollTop;
+///import baidu.page.getViewWidth;
+///import baidu.page.getViewHeight;
 
 /**
  * 将控件或者指定元素与指定的元素对齐
@@ -30,18 +34,27 @@ baidu.ui.behavior.posable.setPositionByElement =
  * @param {HTMLElement|string} element optional 要对齐的元素或元素id，如果不指定，默认为当前控件的主元素.
  * @param {Object} options optional 选项，同setPosition方法.
  */
-baidu.ui.behavior.posable._setPositionByElement =
-    function(target, element, options) {
-        options.once = false;
-        var targetPos = baidu.dom.getPosition(target),
-            coordinate = {};
-
-        options.position && (options.position = options.position.toLowerCase());
-
-        coordinate['x'] = targetPos.left +
-                            (options.position.indexOf('right') >= 0 ? target.offsetWidth : 0);
-        coordinate['y'] = targetPos.top +
-                            (options.position.indexOf('bottom') >= 0 ? target.offsetHeight : 0);
-
-        this._positionByCoordinate(element, coordinate, options);
-    };
+baidu.ui.behavior.posable._setPositionByElement = function(target, element, options){
+    var targetPos = baidu.dom.getPosition(target),
+        scrollPos = {left: baidu.page.getScrollLeft(), top: baidu.page.getScrollTop()},
+        viewSize = {width: baidu.page.getViewWidth(), height: baidu.page.getViewHeight()};
+        
+    function getPosition(type){
+        var val = 0,
+            axis = {
+                x: {pos: 'left', size: 'width', offset: 'offsetWidth'},
+                y: {pos: 'top', size: 'height', offset: 'offsetHeight'}
+            }[type];
+        if(options.position.indexOf('x' == type ? 'right' : 'bottom') > -1){
+            val = targetPos[axis.pos] + target[axis.offset] + element[axis.offset] - scrollPos[axis.pos] > 
+                viewSize[axis.size] ? targetPos[axis.pos] - element[axis.offset]
+                : targetPos[axis.pos] + target[axis.offset]
+        }else{
+            val = targetPos[axis.pos] + element[axis.offset] - scrollPos[axis.pos] > 
+                viewSize[axis.size] ? targetPos[axis.pos] - element[axis.offset] + target[axis.offset]
+                : targetPos[axis.pos];
+        }
+        return val;
+    }
+    this._positionByCoordinate(element, {x: getPosition('x'), y: getPosition('y')}, options);
+}
