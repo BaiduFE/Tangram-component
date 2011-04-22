3d9df9a7288b8f2ed72c4ffea453d7669c399672
tianlili <tianlili3283@gmail.com>
2011-04-22 15:11:42 +0800

    添加storage和setPositionByElement的用例

--- test/baidu/ui/behavior/posable/setPositionByElement.js
+++ test/baidu/ui/behavior/posable/setPositionByElement.js
@@ -87,8 +87,7 @@ test('options', function() {
 	});
 });
 
-test('options.position', function() {
-
+test('options.position, lefttop', function() {
 	var div0 = document.body.appendChild(document.createElement('div')), 
 	div1 = document.body.appendChild(document.createElement('div')),
 	ui = {
@@ -97,7 +96,7 @@ test('options.position', function() {
 	};
 	$(document.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
 	$(div0).css('position', 'absolute').css('left', 20).css('top', 20).css(
-			'width', 10).css('height', 10)
+			'width', 20).css('height', 20)
 			.css('background-color', 'yellow').attr('id', 'div0');
 	$(div1).css('width', 10).css('height', 10).css('background-color',
 			'blue').css('position', 'absolute').attr('id', 'div1');
@@ -106,9 +105,164 @@ test('options.position', function() {
 		ui[key] = baidu.ui.behavior.posable[key];
 	ui.setPositionByElement(div0, div1, {
 		once : true,
-		position : 'lefttop'
+		position : ''
 	});
-	
 	equals(parseInt(div1.style.left), parseInt(div0.style.left), 'check left');
 	equals(parseInt(div1.style.top), parseInt(div0.style.top), 'check top');
+	document.body.removeChild(div0);
+	document.body.removeChild(div1);
+});
+
+test('options.position, rightbottom', function() {
+	var div0 = document.body.appendChild(document.createElement('div')), 
+	div1 = document.body.appendChild(document.createElement('div')),
+	ui = {
+		dispatchEvent : function(type) {
+		}
+	};
+	$(document.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
+	$(div0).css('position', 'absolute').css('left', 20).css('top', 20).css(
+			'width', 20).css('height', 20)
+			.css('background-color', 'yellow').attr('id', 'div0');
+	$(div1).css('width', 10).css('height', 10).css('background-color',
+			'blue').css('position', 'absolute').attr('id', 'div1');
+	for ( var key in baidu.ui.behavior.posable)
+		// ui extend posable
+		ui[key] = baidu.ui.behavior.posable[key];
+	ui.setPositionByElement(div0, div1, {
+		once : true,
+		position : 'rightbottom'
+	});
+	equals(parseInt(div1.style.left), parseInt(div0.style.left) + parseInt(div0.style.width), 'check left');
+	equals(parseInt(div1.style.top), parseInt(div0.style.top)+ parseInt(div0.style.height), 'check top');
+	document.body.removeChild(div0);
+	document.body.removeChild(div1);
+});
+
+test('test in a small iframe, lefttop, small element', function() {
+	ua.frameExt(function(w, f) {
+		$(f).css('width', 100).css('height', 100);
+		var doc = w.document, div0 = doc.body.appendChild(doc
+				.createElement('div')), div1 = doc.body.appendChild(doc
+				.createElement('div')), ui = {
+			dispatchEvent : function(type) {
+			}
+		};
+		$(doc.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
+		$(div0).css('position', 'absolute').css('left', 100).css('top', 100).css(
+				'width', 20).css('height', 20)
+				.css('background-color', 'yellow').attr('id', 'div0');
+		$(div1).css('width', 10).css('height', 10).css('position', 'absolute').css('background-color',
+				'blue').attr('id', 'div1');
+		for ( var key in w.baidu.ui.behavior.posable)
+			// ui extend posable
+			ui[key] = w.baidu.ui.behavior.posable[key];
+//		w.scrollTo(100,100);
+		ui.setPositionByElement(div0, div1, {
+			once : true,
+			position : ''
+		});
+		equals(parseInt(div1.style.left), parseInt(div0.style.left), 'check left');
+		equals(parseInt(div1.style.top), parseInt(div0.style.top), 'check top');
+		doc.body.removeChild(div0);
+		doc.body.removeChild(div1);
+		te.obj.push(f);
+		this.finish();
+	});
+});
+
+test('test in a small iframe, lefttop, big element', function() {
+	ua.frameExt(function(w, f) {
+		$(f).css('width', 100).css('height', 100);
+		var doc = w.document, div0 = doc.body.appendChild(doc
+				.createElement('div')), div1 = doc.body.appendChild(doc
+				.createElement('div')), ui = {
+			dispatchEvent : function(type) {
+			}
+		};
+		$(doc.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
+		$(div0).css('position', 'absolute').css('left', 10).css('top', 10).css(
+				'width', 100).css('height', 100)
+				.css('background-color', 'yellow').attr('id', 'div0');
+		$(div1).css('width', 10).css('height', 10).css('position', 'absolute').css('background-color',
+				'blue').attr('id', 'div1');
+		for ( var key in w.baidu.ui.behavior.posable)
+			// ui extend posable
+			ui[key] = w.baidu.ui.behavior.posable[key];
+		w.scrollTo(100,100);
+		ui.setPositionByElement(div0, div1, {
+			once : true,
+			position : ''
+		});
+		equals(parseInt(div1.style.left), parseInt(div0.style.left), 'check left');
+		equals(parseInt(div1.style.top), parseInt(div0.style.top), 'check top');
+		doc.body.removeChild(div0);
+		doc.body.removeChild(div1);
+		te.obj.push(f);
+		this.finish();
+	});
+});
+
+test('test in a small iframe, bottomright, small element', function() {
+	ua.frameExt(function(w, f) {
+		$(f).css('width', 100).css('height', 100);
+		var doc = w.document, div0 = doc.body.appendChild(doc
+				.createElement('div')), div1 = doc.body.appendChild(doc
+				.createElement('div')), ui = {
+			dispatchEvent : function(type) {
+			}
+		};
+		$(doc.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
+		$(div0).css('position', 'absolute').css('left', 100).css('top', 100).css(
+				'width', 20).css('height', 20)
+				.css('background-color', 'yellow').attr('id', 'div0');
+		$(div1).css('width', 10).css('height', 10).css('position', 'absolute').css('background-color',
+				'blue').attr('id', 'div1');
+		for ( var key in w.baidu.ui.behavior.posable)
+			// ui extend posable
+			ui[key] = w.baidu.ui.behavior.posable[key];
+		w.scrollTo(100,100);
+		ui.setPositionByElement(div0, div1, {
+			once : true,
+			position : 'bottomright'
+		});
+		equals(parseInt(div1.style.left), parseInt(div0.style.left) + parseInt(div0.style.width), 'check left');
+		equals(parseInt(div1.style.top), parseInt(div0.style.top)+ parseInt(div0.style.height), 'check top');
+		doc.body.removeChild(div0);
+		doc.body.removeChild(div1);
+		te.obj.push(f);
+		this.finish();
+	});
+});
+
+test('test in a small iframe, bottomright, big element', function() {
+	ua.frameExt(function(w, f) {
+		$(f).css('width', 100).css('height', 100);
+		var doc = w.document, div0 = doc.body.appendChild(doc
+				.createElement('div')), div1 = doc.body.appendChild(doc
+				.createElement('div')), ui = {
+			dispatchEvent : function(type) {
+			}
+		};
+		$(doc.body).css('margin', 0).css('padding', 0).css('borderWidth', 0);// 这个必须
+		$(div0).css('position', 'absolute').css('left', 10).css('top', 10).css(
+				'width', 100).css('height', 100)
+				.css('background-color', 'yellow').attr('id', 'div0');
+		$(div1).css('width', 10).css('height', 10).css('position', 'absolute').css('background-color',
+				'blue').attr('id', 'div1');
+		for ( var key in w.baidu.ui.behavior.posable)
+			// ui extend posable
+			ui[key] = w.baidu.ui.behavior.posable[key];
+		w.scrollTo(100,100);
+		ui.setPositionByElement(div0, div1, {
+			once : true,
+			position : 'bottomright'
+		});
+		equals(parseInt(div1.style.left), parseInt(div0.style.left) + parseInt(div0.style.width), 'check left');
+		equals(parseInt(div1.style.top), parseInt(div0.style.top)+ parseInt(div0.style.height), 'check top');
+		doc.body.removeChild(div0);
+		doc.body.removeChild(div1);
+		te.obj.push(f);
+		this.finish();
+	});
 });
