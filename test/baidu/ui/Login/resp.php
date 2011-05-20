e11410067b08736947c3fe2fba9a13b83e45d2fe
dailiqi <dailiqi@baidu.com>
2011-05-20 17:31:18 +0800

    ui.Login : resp.php 路径写法写成"/" - [Testcase] 在linux中报错

--- test/baidu/ui/Login/resp.php
+++ test/baidu/ui/Login/resp.php
@@ -1,6 +1,6 @@
 <?php 
 $path = dirname(__FILE__);
-$handle = fopen($path."\\resp.js", "r") or exit("Unable to open file!");
+$handle = fopen($path."/resp.js", "r") or exit("Unable to open file!");
 while(!feof($handle)){
 	$line .= fgets($handle);
 }
