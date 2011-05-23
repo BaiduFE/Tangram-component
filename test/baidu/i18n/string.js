46833201eb5a72eed97bb7e75644ce9d727ecc90
tianlili <tianlili3283@gmail.com>
2011-05-23 20:02:06 +0800

    添加i18n的用例
    [Testcase] 添加i18n的用例
    currency : 添加用例
    date ：添加用例
    number ： 添加用例
    string ： 添加用例

---
+++ test/baidu/i18n/string.js
@@ -0,0 +1,30 @@
+module("baidu.i18n.string");
+
+test('trim', function(){
+	stop();
+	ua.importsrc('baidu.i18n.cultures.en-US,baidu.i18n.cultures.zh-CN', function(){
+		equals(baidu.i18n.string.trim('zh-CN', ''), '', 'check ""');
+		equals(baidu.i18n.string.trim('zh-CN',' '), '', 'check " "');
+		equals(baidu.i18n.string.trim('zh-CN','    1fsdgs   '), '1fsdgs', 'check "1fsdgs"');
+		equals(baidu.i18n.string.trim('zh-CN','1fsdgs '), '1fsdgs', 'check "1fsdgs"');
+		equals(baidu.i18n.string.trim('zh-CN',' 1fsdgs'), '1fsdgs', 'check "1fsdgs"');
+		equals(baidu.i18n.string.trim('en-US', ''), '', 'check ""');
+		equals(baidu.i18n.string.trim('en-US',' '), '', 'check " "');
+		equals(baidu.i18n.string.trim('en-US','   1fsdgs   '), '1fsdgs', 'check "1fsdgs"');
+		equals(baidu.i18n.string.trim('en-US','1fsdgs '), '1fsdgs', 'check "1fsdgs"');
+		equals(baidu.i18n.string.trim('en-US',' 1fsdgs'), '1fsdgs', 'check "1fsdgs"');
+		start();
+	}, 'baidu.i18n.cultures.en-US','baidu.i18n.string');
+});
+
+test('translation', function(){
+
+	equals(baidu.i18n.string.translation('zh-CN', 'ok'), '确定', 'en-US to zh-CN');
+	equals(baidu.i18n.string.translation('zh-CN', 'cancel'), '取消', 'en-US to zh-CN');
+	equals(baidu.i18n.string.translation('zh-CN', 'signin'), '注册', 'en-US to zh-CN');
+	equals(baidu.i18n.string.translation('zh-CN', 'signup'), '登录', 'en-US to zh-CN');
+	equals(baidu.i18n.string.translation('en-US', 'ok'), 'ok', 'en-US to en-US');
+	equals(baidu.i18n.string.translation('en-US', 'cancel'), 'cancel', 'en-US to en-US');
+	equals(baidu.i18n.string.translation('en-US', 'signin'), 'signin', 'en-US to en-US');
+	equals(baidu.i18n.string.translation('en-US', 'signup'), 'signup', 'en-US to en-US');
+});
