968e34c774431507131a0ca792d054935fb94605
tianlili <tianlili3283@gmail.com>
2011-06-02 17:46:30 +0800

    修改之前fail的用例
    [Testcase] 修改之前fail的用例
    Dialog$coverable ：测试机上已有可以解决flash播放问题的播放器，取消代码屏蔽

--- test/baidu/ui/Dialog/Dialog$coverable.js
+++ test/baidu/ui/Dialog/Dialog$coverable.js
@@ -141,182 +141,181 @@ test('close, show select',function(){
 	document.body.removeChild(select_a);
 });
 
-//----------------为了保证用例在测试机上运行通过，暂时屏蔽此用例   begin----------------------------
-//test('onopen, hide flash, transparent smartCover',function(){
-//	expect(4);
-//	stop();
-//	var check = function(){
-//		var options = {
-//				titleText : "title",
-//				contentText : "content",
-//				top : '50',
-//				left : '50',
-//				width : 300,
-//				height :300
-//			};
-//		var d = new baidu.ui.Dialog(options);
-//		d.render();
-//		var div = document.createElement('div');
-//		div.id = 'flashContainer1';
-//		document.body.appendChild(div);
-//		baidu.swf.create({
-//            id: "flash1",
-//            url: upath + 'flash/test_flash.swf',
-//            width:695,
-//            height:90,
-//            wmode:'window'
-//        }, "flashContainer1");
-//		var div2 = document.createElement('div');
-//		div2.id = 'flashContainer2';
-//		document.body.appendChild(div2);
-//		baidu.swf.create({
-//            id: "flash1",
-//            url: upath + 'flash/test_flash.swf',
-//            width:695,
-//            height:90,
-//            wmode:'transparent'
-//        }, "flashContainer2");
-//		d.open();
-//		ok(!d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is transparent');
-//		equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
-//		ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
-//		ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
-//		te.obj.push(d);
-//		document.body.removeChild(div);
-//		document.body.removeChild(div2);
-//		start();
-//	};
-//	ua.importsrc('baidu.swf.create', 
-//			check ,'baidu.swf.create', 'baidu.ui.Dialog.Dialog$coverable');
-//});
-//
-//test('onopen, hide flash, transparent smartCover, red dialog',function(){
-//	expect(5);
-//	var options = {
-//			titleText : "title",
-//			contentText : "content",
-//			top : '50',
-//			left : '50',
-//			width : 300,
-//			height :300
-//		};
-//	var d = new baidu.ui.Dialog(options);
-//	d.render();
-//	$(d.getMain()).css('backgroundColor', "red");
-//	var div = document.createElement('div');
-//	div.id = 'flashContainer1';
-//	document.body.appendChild(div);
-//	baidu.swf.create({
-//        id: "flash1",
-//        url: upath + 'flash/test_flash.swf',
-//        width:695,
-//        height:90,
-//        wmode:'window'
-//    }, "flashContainer1");
-//	var div2 = document.createElement('div');
-//	div2.id = 'flashContainer2';
-//	document.body.appendChild(div2);
-//	baidu.swf.create({
-//        id: "flash1",
-//        url: upath + 'flash/test_flash.swf',
-//        width:695,
-//        height:90,
-//        wmode:'transparent'
-//    }, "flashContainer2");
-//	d.open();
-//	ok(!d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is transparent');
-//	ok(d.getMain().style['backgroundColor'] == 'red' || d.getMain().style['backgroundColor'] == '#ff0000', 'The table is red');
-//	equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
-//	ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
-//	ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
-//	te.obj.push(d);
-//	document.body.removeChild(div);
-//	document.body.removeChild(div2);
-//});
-//
-//test('onopen, hide flash, white smartCover',function(){
-//	expect(4);
-//	var options = {
-//			titleText : "title",
-//			contentText : "content",
-//			top : '50',
-//			left : '50',
-//			width : 300,
-//			height :300,
-//			coverableOptions : {
-//				color : 'white'
-//			}
-//		};
-//	if(baidu.browser.ie){
-//		baidu.extend(options, {
-//			coverableOptions : {
-//				color : 'white',
-//				opacity : 100
-//			}
-//		});
-//	}
-//	var d = new baidu.ui.Dialog(options);
-//	d.render();
-//	var div = document.createElement('div');
-//	div.id = 'flashContainer1';
-//	document.body.appendChild(div);
-//	baidu.swf.create({
-//        id: "flash1",
-//        url: upath + 'flash/test_flash.swf',
-//        width:695,
-//        height:90,
-//        wmode:'window'
-//    }, "flashContainer1");
-//	var div2 = document.createElement('div');
-//	div2.id = 'flashContainer2';
-//	document.body.appendChild(div2);
-//	baidu.swf.create({
-//        id: "flash1",
-//        url: upath + 'flash/test_flash.swf',
-//        width:695,
-//        height:90,
-//        wmode:'transparent'
-//    }, "flashContainer2");
-//	d.open();
-//	ok(d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is not transparent');
-//	equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
-//	ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
-//	ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
-//	te.obj.push(d);
-//	document.body.removeChild(div);
-//	document.body.removeChild(div2);
-//});
-//
-//test('close, hide flash',function(){
-//	expect(2);
-//	var options = {
-//			titleText : "title",
-//			contentText : "content",
-//			top : '50',
-//			left : '50',
-//			width : 300,
-//			height :300
-//		};
-//	var d = new baidu.ui.Dialog(options);
-//	d.render();
-//	var div = document.createElement('div');
-//	div.id = 'flashContainer1';
-//	document.body.appendChild(div);
-//	baidu.swf.create({
-//        id: "flash1",
-//        url: upath + 'flash/test_flash.swf',
-//        width:695,
-//        height:90,
-//        wmode:'window'
-//    }, "flashContainer1");
-//	d.open();
-//	d.close();
-//	equals(d.getMain().firstChild.firstChild.style.display, 'none', 'The style.display of the iframe is none');
-//	ok(!isShown(d.getMain().firstChild.firstChild) ,'The iframe is hidden');
-//	te.obj.push(d);
-//	document.body.removeChild(div);
-//});
-//----------------为了保证用例在测试机上运行通过，暂时屏蔽此用例   end----------------------------
+
+test('onopen, hide flash, transparent smartCover',function(){
+	expect(4);
+	stop();
+	var check = function(){
+		var options = {
+				titleText : "title",
+				contentText : "content",
+				top : '50',
+				left : '50',
+				width : 300,
+				height :300
+			};
+		var d = new baidu.ui.Dialog(options);
+		d.render();
+		var div = document.createElement('div');
+		div.id = 'flashContainer1';
+		document.body.appendChild(div);
+		baidu.swf.create({
+            id: "flash1",
+            url: upath + 'flash/test_flash.swf',
+            width:695,
+            height:90,
+            wmode:'window'
+        }, "flashContainer1");
+		var div2 = document.createElement('div');
+		div2.id = 'flashContainer2';
+		document.body.appendChild(div2);
+		baidu.swf.create({
+            id: "flash1",
+            url: upath + 'flash/test_flash.swf',
+            width:695,
+            height:90,
+            wmode:'transparent'
+        }, "flashContainer2");
+		d.open();
+		ok(!d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is transparent');
+		equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
+		ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
+		ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
+		te.obj.push(d);
+		document.body.removeChild(div);
+		document.body.removeChild(div2);
+		start();
+	};
+	ua.importsrc('baidu.swf.create', 
+			check ,'baidu.swf.create', 'baidu.ui.Dialog.Dialog$coverable');
+});
+
+test('onopen, hide flash, transparent smartCover, red dialog',function(){
+	expect(5);
+	var options = {
+			titleText : "title",
+			contentText : "content",
+			top : '50',
+			left : '50',
+			width : 300,
+			height :300
+		};
+	var d = new baidu.ui.Dialog(options);
+	d.render();
+	$(d.getMain()).css('backgroundColor', "red");
+	var div = document.createElement('div');
+	div.id = 'flashContainer1';
+	document.body.appendChild(div);
+	baidu.swf.create({
+        id: "flash1",
+        url: upath + 'flash/test_flash.swf',
+        width:695,
+        height:90,
+        wmode:'window'
+    }, "flashContainer1");
+	var div2 = document.createElement('div');
+	div2.id = 'flashContainer2';
+	document.body.appendChild(div2);
+	baidu.swf.create({
+        id: "flash1",
+        url: upath + 'flash/test_flash.swf',
+        width:695,
+        height:90,
+        wmode:'transparent'
+    }, "flashContainer2");
+	d.open();
+	ok(!d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is transparent');
+	ok(d.getMain().style['backgroundColor'] == 'red' || d.getMain().style['backgroundColor'] == '#ff0000', 'The table is red');
+	equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
+	ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
+	ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
+	te.obj.push(d);
+	document.body.removeChild(div);
+	document.body.removeChild(div2);
+});
+
+test('onopen, hide flash, white smartCover',function(){
+	expect(4);
+	var options = {
+			titleText : "title",
+			contentText : "content",
+			top : '50',
+			left : '50',
+			width : 300,
+			height :300,
+			coverableOptions : {
+				color : 'white'
+			}
+		};
+	if(baidu.browser.ie){
+		baidu.extend(options, {
+			coverableOptions : {
+				color : 'white',
+				opacity : 100
+			}
+		});
+	}
+	var d = new baidu.ui.Dialog(options);
+	d.render();
+	var div = document.createElement('div');
+	div.id = 'flashContainer1';
+	document.body.appendChild(div);
+	baidu.swf.create({
+        id: "flash1",
+        url: upath + 'flash/test_flash.swf',
+        width:695,
+        height:90,
+        wmode:'window'
+    }, "flashContainer1");
+	var div2 = document.createElement('div');
+	div2.id = 'flashContainer2';
+	document.body.appendChild(div2);
+	baidu.swf.create({
+        id: "flash1",
+        url: upath + 'flash/test_flash.swf',
+        width:695,
+        height:90,
+        wmode:'transparent'
+    }, "flashContainer2");
+	d.open();
+	ok(d.getMain().firstChild.firstChild.style['backgroundColor'],'The iframe is not transparent');
+	equals(d.getMain().firstChild.firstChild.style['zIndex'], '-1', 'The z-index of the iframe is -1')
+	ok(Math.abs(d.getMain().firstChild.firstChild.offsetWidth - d.getBody().offsetWidth) < 5, 'The width of the iframe is right');
+	ok(Math.abs(d.getMain().firstChild.firstChild.offsetHeight - d.getBody().offsetHeight * 0.97) < 5, 'The Height of the iframe is right');
+	te.obj.push(d);
+	document.body.removeChild(div);
+	document.body.removeChild(div2);
+});
+
+test('close, hide flash',function(){
+	expect(2);
+	var options = {
+			titleText : "title",
+			contentText : "content",
+			top : '50',
+			left : '50',
+			width : 300,
+			height :300
+		};
+	var d = new baidu.ui.Dialog(options);
+	d.render();
+	var div = document.createElement('div');
+	div.id = 'flashContainer1';
+	document.body.appendChild(div);
+	baidu.swf.create({
+        id: "flash1",
+        url: upath + 'flash/test_flash.swf',
+        width:695,
+        height:90,
+        wmode:'window'
+    }, "flashContainer1");
+	d.open();
+	d.close();
+	equals(d.getMain().firstChild.firstChild.style.display, 'none', 'The style.display of the iframe is none');
+	ok(!isShown(d.getMain().firstChild.firstChild) ,'The iframe is hidden');
+	te.obj.push(d);
+	document.body.removeChild(div);
+});
 
 test('onupdate, hide select',function(){
 	expect(12);
